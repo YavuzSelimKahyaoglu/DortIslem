54fffa77fc714c46dd538134064dbb8e00b3c722
Yavuz Selim Kahyaoğlu <68874461+YavuzSelimKahyaoglu@users.noreply.github.com>
2021-01-22 20:28:45 +0300

    Temel Kodlar Yazıldı.

--- app/src/main/java/com/yaseka/dortislem/MainActivity.kt
+++ app/src/main/java/com/yaseka/dortislem/MainActivity.kt
@@ -2,10 +2,48 @@ package com.yaseka.dortislem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sum(view: View){
+        var sayi1=sayi1Text.text.toString().toInt();
+        var sayi2=sayi2Text.text.toString().toInt();
+
+        var sonuc=(sayi1+sayi2).toString();
+
+        sonucText.text="Sonuç: ${sonuc}";
+    }
+
+    fun sub(view: View){
+        var sayi1=sayi1Text.text.toString().toInt();
+        var sayi2=sayi2Text.text.toString().toInt();
+
+        var sonuc=(sayi1-sayi2).toString();
+
+        sonucText.text="Sonuç: ${sonuc}";
+    }
+
+    fun multiply(view: View){
+        var sayi1=sayi1Text.text.toString().toInt();
+        var sayi2=sayi2Text.text.toString().toInt();
+
+        var sonuc=(sayi1*sayi2).toString();
+
+        sonucText.text="Sonuç: ${sonuc}";
+    }
+
+    fun divide(view: View){
+        var sayi1=sayi1Text.text.toString().toDouble();
+        var sayi2=sayi2Text.text.toString().toDouble();
+
+        var sonuc=(sayi1/sayi2).toString();
+
+        sonucText.text="Sonuç: ${sonuc}";
     }
 }
